102330acd059d6b0b34cf374a4a1450ff36677d8
MatteoGevi <38760501+DataStrander@users.noreply.github.com>
2021-08-05 17:12:32 +0200

    Start

--- InitScrapping.py
+++ InitScrapping.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 # ---
 # jupyter:
 #   jupytext:
@@ -12,8 +13,23 @@
 #     name: python3
 # ---
 
-import re
+import requests
+import urllib.request
+import time
 from bs4 import BeautifulSoup
-import pandas as pd
+
+# +
+'''HTML Analysis'''
+
+url = 'https://www.vizedex.com/'
+response = requests.get(url)
+# If while printing response you get 200, it means that the code went through
 
 
+
+# +
+'''Text Parsing'''
+
+# Next we parse the html with BeautifulSoup so that we can work with a nicer, nested BeautifulSoup data structure.
+soup = BeautifulSoup(response.text, “html.parser”)
+soup.findAll('a')
