02cce2df4e5c177de07ff2e022fc9f6e0a36e879
MatteoGevi <38760501+DataStrander@users.noreply.github.com>
2021-08-05 17:28:59 +0200

    Reorder Text

--- .ipynb_checkpoints/InitScrapping-checkpoint.py
+++ .ipynb_checkpoints/InitScrapping-checkpoint.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 # ---
 # jupyter:
 #   jupytext:
@@ -12,8 +13,30 @@
 #     name: python3
 # ---
 
+# ## VizEd Website Scrapping
+# This analysis is used to test how many information are exploitable through Web Scrapping on our StartUp Website
+# Author: Matteo Gevi
+# Property: VizEd UG
+
+import requests
+import urllib.request
+import time
 import re
 from bs4 import BeautifulSoup
-import pandas as pd
+
+# +
+'''HTML Analysis'''
+
+url = 'https://www.vizedex.com/'
+response = requests.get(url)
+# If while printing response you get 200, it means that the code went through
+
+# +
+'''Text Parsing'''
+
+# Next we parse the html with BeautifulSoup so that we can work with a nicer, nested BeautifulSoup data structure.
+soup = BeautifulSoup(response.text, “html.parser”)
+soup.findAll('a')
+# -
 
 
--- InitScrapping.py
+++ InitScrapping.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 # ---
 # jupyter:
 #   jupytext:
@@ -22,3 +23,20 @@ import urllib.request
 import time
 import re
 from bs4 import BeautifulSoup
+
+# +
+'''HTML Analysis'''
+
+url = 'https://www.vizedex.com/'
+response = requests.get(url)
+# If while printing response you get 200, it means that the code went through
+
+# +
+'''Text Parsing'''
+
+# Next we parse the html with BeautifulSoup so that we can work with a nicer, nested BeautifulSoup data structure.
+soup = BeautifulSoup(response.text, “html.parser”)
+soup.findAll('a')
+# -
+
+
