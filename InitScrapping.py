095a7b437482cd82c192b78b58286994e427c192
MatteoGevi <38760501+DataStrander@users.noreply.github.com>
2021-08-05 17:11:58 +0200

    Update InitScrapping.py

--- InitScrapping.py
+++ InitScrapping.py
@@ -11,7 +11,16 @@
 #     language: python
 #     name: python3
 # ---
+#
+# ## VizEd Website Scrapping
+# This analysis is used to test how many information are exploitable through Web Scrapping on our StartUp Website
+# Author: Matteo Gevi
+# Property: VizEd UG
 
+# %% [markdown]
+## Import
+
+# %%
 import re
 from bs4 import BeautifulSoup
 import pandas as pd
